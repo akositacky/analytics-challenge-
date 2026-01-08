042a38fc3b18a0e119c4f1bfcea54ea64c7ad7d6
Jerome John Tactac <jerome.m.tactac@gmail.com>
2026-01-08 11:44:34 +0800

    add margin

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -86,7 +86,7 @@ export default function LoginPage() {
               />
             </div>
           </CardContent>
-          <CardFooter className="flex flex-col space-y-4">
+          <CardFooter className="flex flex-col space-y-4 mt-5">
             <Button type="submit" className="w-full" disabled={loading}>
               {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
               Sign in
